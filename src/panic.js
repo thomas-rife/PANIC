54bde0a7c7e8982d16302d3c3e15078b361d9879
Thomas Rife <112517908+thomas-rife@users.noreply.github.com>
2025-02-03 12:21:30 -0800

    adding grammar basic updates

--- src/panic.js
+++ src/panic.js
@@ -1,5 +1,22 @@
-console.log("Hello");
+import * as fs from "fs";
+import * as ohm from "ohm-js";
 
-export function parse(input) {
-  return "I do not work";
+// Read the contents of the file cassowary.ohm into a string
+const grammar = ohm.grammar(fs.readFileSync("src/cassowary.ohm", "utf8"));
+
+const interpreter = grammar.createSemantics();
+interpreter.addOperation("eval", {
+  Program(statements) {
+    for (const statement of statments.children) {
+      statement.eval();
+    }
+  },
+});
+
+const sourceCode = process.argv[2];
+const match = grammar.match(sourceCode);
+if (match.succeeded()) {
+  interpreter(match).eval();
+} else {
+  console.error(match.message);
 }
--- test/compiler.test.js
+++ test/compiler.test.js
@@ -1,9 +1,9 @@
 import { describe, it } from "node:test";
 import { deepEqual } from "node:assert/strict";
-import { parse } from "../src/panic.js";
+import { parse } from "../src/cassowary.js";
 
 describe("Interpreter", () => {
-  it("Parses correctly", () => {
+  it("parses correctly", () => {
     deepEqual(parse(), "I do not work");
   });
 });
