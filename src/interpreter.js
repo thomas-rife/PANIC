bfbaae64ea238d3ff9ef11be233d3c0facf37e00
Thomas Rife <thomasrife@Thomass-MacBook-Pro.local>
2025-02-10 11:47:37 -0800

    updating interpreter

--- src/interpreter.js
+++ src/interpreter.js
@@ -2,6 +2,7 @@
 
 export default function interpret(match) {
   const grammar = match.matcher.grammar;
+
   const memory = new Map();
 
   const interpreter = grammar.createSemantics().addOperation("eval", {
@@ -10,30 +11,78 @@ export default function interpret(match) {
         statement.eval();
       }
     },
-
-    Stmt_increment(_op, id, _semiColon) {},
-
-    VarDec(_letKw, id, _eq, exp, _semiColon) {
-      memory.set(id.eval(), exp.eval());
+    Stmt_increment(_op, id, _semi) {
+      const value = id.eval();
+      memory.set(id.sourceString, value + 1);
     },
-
-    PrintStmt(_print, exp, _semiColon) {
-      // underscore means we don't care about the value
+    VarDec(_let, id, _eq, exp, _semi) {
+      memory.set(id.sourceString, exp.eval());
+    },
+    PrintStmt(_print, exp, _semi) {
       console.log(exp.eval());
     },
-
-    numeral(digits) {
-      return Number(digits.sourceString);
+    AssignmentStmt(id, _eq, exp, _semi) {
+      const value = exp.eval();
+      const variable = id.eval(); // Call id eval to check if the variable exists
+      memory.set(id.sourceString, value);
     },
-
-    id(first, rest) {
-      // the keyword and ids are checked by the grammar, no check needed here
-      const name = `${first.sourceString}${rest.sourceString}`;
+    WhileStmt(_while, exp, block) {
+      while (exp.eval()) {
+        block.eval();
+      }
+    },
+    Block(_open, statements, _close) {
+      for (const statement of statements.children) {
+        statement.eval();
+      }
+    },
+    Exp_test(left, op, right) {
+      switch (op.sourceString) {
+        case "<":
+          return left.eval() < right.eval();
+        case ">":
+          return left.eval() > right.eval();
+        case "<=":
+          return left.eval() <= right.eval();
+        case ">=":
+          return left.eval() >= right.eval();
+        case "==":
+          return left.eval() === right.eval();
+        case "!=":
+          return left.eval() !== right.eval();
+        default:
+          throw new Error(`What are you doing? There is no ${op.sourceString}`);
+      }
+    },
+    Condition_add(left, _op, right) {
+      return left.eval() + right.eval();
+    },
+    Condition_sub(left, _op, right) {
+      return left.eval() - right.eval();
+    },
+    Term_mul(left, _op, right) {
+      return left.eval() * right.eval();
+    },
+    Term_div(left, _op, right) {
+      return left.eval() / right.eval();
+    },
+    Term_mod(left, _op, right) {
+      return left.eval() % right.eval();
+    },
+    Primary_parens(_open, exp, _close) {
+      return exp.eval();
+    },
+    numeral(digits, _dot, _fractional, _e, _sign, _exponent) {
+      return Number(this.sourceString);
+    },
+    id(_first, _rest) {
+      const name = this.sourceString;
       if (!memory.has(name)) {
-        throw new Error(`Variable ${name} not defined`);
+        throw new Error(`What are you doing? There is no ${name}`);
       }
       return memory.get(name);
     },
   });
-  throw interpreter(match.eval());
+
+  throw interpreter(match).eval();
 }
--- src/parser.js
+++ src/parser.js
@@ -4,7 +4,7 @@ import * as ohm from "ohm-js";
 const grammar = ohm.grammar(fs.readFileSync("src/panic.ohm", "utf8"));
 
 // takes file name and raeds it into a string, tries to match it with grammar
-export function parse(sourceCodeFileName) {
+export default function parse(sourceCodeFileName) {
   const sourceCode = fs.readFileSync(sourceCodeFileName, "utf8");
   const match = grammar.match(sourceCode);
   if (match.failed()) {
