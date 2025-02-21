4e3a3f83d68b9308f6d7b2b58e22888142769589
Thomas Rife <thomasrife@Mac.lan>
2025-02-20 20:08:06 -0800

    updating grammar

--- src/panic.js
+++ src/panic.js
@@ -5,7 +5,6 @@
 
 import * as fs from "fs";
 import parse from "./parser.js";
-import translate from "./translator.js";
 
 // Read the contents of the file grammar.ohm into a string
 if (process.argv.length !== 3) {
@@ -17,11 +16,10 @@ try {
   // synax
   const sourceCode = fs.readFileSync(process.argv[2], "utf8");
   const match = parse(sourceCode);
-  const target = translate(match);
-  console.log(target.join("\n"));
-// } catch (e) {
-//   console.error(`${e}`);
-  process.exit(1);
-// }
-// interpreter vs compiler: translates it into something else
 
+  console.log(match.join("\n"));
+} catch (e) {
+  console.error(`${e}`);
+  process.exit(1);
+}
+// interpreter vs compiler: translates it into something else
