918c7efabd58717e05ad295869d6161c19d067dd
Brian Bowers <bmbowers22@gmail.com>
2025-03-18 15:20:26 -0700

    somehow working again

--- src/panic.js
+++ src/panic.js
@@ -17,19 +17,14 @@ Prints to stdout according to <outputType>, which must be one of:
 `;
 
 async function compileFromFile(filename, outputType) {
-  // try {
-  const buffer = await fs.readFile(filename);
-  const compiled = compile(buffer.toString(), outputType);
-
-  console.log("Compiled object type:", typeof compiled);
-  console.log("Compiled object:", compiled);
-  console.log("Debugging stringify input:", JSON.stringify(compiled, null, 2));
-
-  console.log(stringify(compiled, "kind") || compiled);
-  // } catch (e) {
-  //   console.error(`\u001b[31m${e}\u001b[39m`);
-  //   process.exitCode = 1;
-  // // }
+  try {
+    const buffer = await fs.readFile(filename);
+    const compiled = compile(buffer.toString(), outputType);
+    console.log(stringify(compiled, "kind") || compiled);
+  } catch (e) {
+    console.error(`\u001b[31m${e}\u001b[39m`);
+    process.exitCode = 1;
+  }
 }
 
 if (process.argv.length !== 4) {
